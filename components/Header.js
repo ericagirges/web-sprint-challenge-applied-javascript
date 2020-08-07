ffca83c52217d63ef58e79d64eba7f0662903de4
Erica Girges <ericafiorenza@gmail.com>
2020-08-07 08:24:50 -0700

    completed header.js

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,37 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+    //create required elements
+    const headerDiv = document.createElement("div");
+    const spanDate = document.createElement("span");
+    const title = document.createElement("h1");
+    const spanTemp = document.createElement("span");
+
+    //assign classes and text content accordingly
+    headerDiv.classList.add("header");
+    spanDate.classList.add("date");
+    spanDate.textContent = "March 28, 2020"
+    title.textContent = "Lambda Times"
+    spanTemp.classList.add("temp");
+    spanTemp.textContent = "98\u00B0"
+    
+   //nest elements appropriately
+   headerDiv.appendChild(spanDate);
+   headerDiv.appendChild(title);
+   headerDiv.appendChild(spanTemp);
+
+   //locate div.header-container in DOM
+   const headerContainer = document.querySelector(".header-container");
+
+   //append new header component to headerContainer
+   headerContainer.appendChild(headerDiv)
+
+   return headerContainer;
+
+}
+
+
+Header();
+
